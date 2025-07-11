1bf1a60abbbebc1fd22f67f237a4fa72e96750d1
Danielle <danielle.shokrian@aol.com>
2025-07-11 12:01:21 -0400

    Arrays lab using inventory management

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,21 @@
 // Write your code here
 
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct () {
+    console.log(products[0]);
+}   
+
+function addProduct (){
+    products.push("Tablet");
+}   
+
+function updateProductName (){
+    products[0] = "Gaming Laptop";
+}   
+
+function removeLastProduct (){
+    products.pop();
+}   
 
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
